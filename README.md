1486b3545754c57fbec047d7ec7977312598a4d0
Stevan Kapicic <kapicic.ste1@gmail.com>
2024-03-22 16:47:33 +0100

    Add docs

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# liblab Terraform Provider 0.0.6
+# liblab Terraform Provider 0.0.8
 
 This repository contains a Terraform provider that allows you to manage resources through the liblab API.
 
--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -31,7 +31,7 @@ type liblabProviderModel struct {
 
 func (p *Provider) Metadata(ctx context.Context, req provider.MetadataRequest, resp *provider.MetadataResponse) {
 	resp.TypeName = "liblab"
-	resp.Version = "0.0.7"
+	resp.Version = "0.0.8"
 }
 
 func (p *Provider) Schema(ctx context.Context, req provider.SchemaRequest, resp *provider.SchemaResponse) {
